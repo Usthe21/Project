b3c464c913c4fdd729c8b4b70df1dff7099ebfbe
Matheus Peron <usthe.dev@gmail.com>
2021-11-03 15:57:38 -0300

    Projets created

---
+++ backend/src/main/java/com/matheusDev/matVendas/MatVendasApplication.java
@@ -0,0 +1,13 @@
+package com.matheusDev.matVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/matheusDev/matVendas/MatVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusDev.matVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
