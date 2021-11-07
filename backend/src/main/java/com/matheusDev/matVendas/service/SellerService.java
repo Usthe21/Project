c83e2120134007cc76eefe3e2379c402b4680087
Matheus Peron <usthe.dev@gmail.com>
2021-11-06 23:09:37 -0300

    Layers

---
+++ backend/src/main/java/com/matheusDev/matVendas/service/SellerService.java
@@ -0,0 +1,25 @@
+package com.matheusDev.matVendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheusDev.matVendas.dto.SellerDTO;
+import com.matheusDev.matVendas.entities.Seller;
+import com.matheusDev.matVendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+	
+	
+}
